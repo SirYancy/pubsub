6b13f3ff7204da4ec122c9731992a1b234a22096
Aparna <mahad028@umn.edu>
2018-02-12 17:43:43 -0600

    integrate UDP with publish

---
+++ pubsub/udp.h
@@ -0,0 +1,21 @@
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <stdbool.h>
+#include <string.h>
+#include <stdio.h>
+#include <arpa/inet.h>
+#include <fcntl.h>
+#include <unistd.h>
+
+#define MAX_BUFFER 1024
+
+#define SERVER_PORT 5678
+#define CLIENT_PORT 6789
+
+bool InitServer(int port, int *sockDesc, struct sockaddr_in *sockAddr);
+bool InitClient(char *IP, int port, int *sockDesc, struct sockaddr_in *sockAddr);
+void Destroy(int sockDesc);
+
+bool SendTo(int sockDesc, struct sockaddr_in *sockAddr, char *buffer);
+int RecvFrom(int sockDesc, struct sockaddr_in *sockAddr, char *buffer);
+
